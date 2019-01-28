74af5bb34ab0cfdbed17adacf00d6929f3496e48
Cassandra Melissa Suarez Cruz <cassandra.suarez.chavez@gmail.com>
2019-01-28 00:14:19 -0800

    Overworld

---
+++ UnitySeniorDesign/Senior Design Project - Copy/Assets/Scripts/Quests/PlayersQuests.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersQuests : MonoBehaviour {
+    public List<Quests> playerQuests = new List<Quests>();
+    public QuestList questList;
+
+    public void QuestCompleted(int ID) {
+        Quests completedQuest = questList.GetByID(ID);
+        playerQuests.Add(completedQuest);
+    }
+}
---
+++ UnitySeniorDesign/Senior Design Project - Copy/Assets/Scripts/Quests/Quests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quests : MonoBehaviour
+{
+    public int ID;
+    public string QuestName;
+    public string QuestDescription;
+    
+
+    public Quests(int ID, string QuestName, string QuestDescription)
+    {
+        this.ID = ID;
+        this.QuestName = QuestName;
+        this.QuestDescription = QuestDescription;
+    }
+
+}
