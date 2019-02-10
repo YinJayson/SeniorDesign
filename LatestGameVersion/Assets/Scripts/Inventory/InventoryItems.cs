210508afc0c71ec90af6ba62b5d6519bf712f3e0
CassandraMelissa <45674823+CassandraMelissa@users.noreply.github.com>
2019-02-10 01:35:25 -0800

    Update InventoryItems.cs

--- LatestGameVersion/Assets/Scripts/Inventory/InventoryItems.cs
+++ LatestGameVersion/Assets/Scripts/Inventory/InventoryItems.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,8 +12,10 @@ public class InventoryItems : MonoBehaviour {
     public int skillStat { get; set; }
     public int attackStat { get; set; }
     public Sprite sprite { get; set; }
+    //item id added for use with quests
+    public int itemID { get; set; }
 
-    public InventoryItems(string itemName, string itemDescription, int healthStat, int attackStat, int skillStat, Sprite icon)
+    public InventoryItems(string itemName, string itemDescription, int healthStat, int attackStat, int skillStat, Sprite icon, int itemID)
     {
         this.itemName = itemName;
         this.itemDescription = itemDescription;
@@ -21,6 +23,7 @@ public class InventoryItems : MonoBehaviour {
         this.attackStat = attackStat;
         this.skillStat = skillStat;
         this.sprite = Resources.Load<Sprite>("Art/Items/" + itemName);
+        this.itemID = itemID;
     }
    
  
