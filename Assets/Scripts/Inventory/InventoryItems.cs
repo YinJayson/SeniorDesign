f0e80481503568941c6dcada147cc462be996357
Cassandra Melissa Suarez Cruz <cassandra.suarez.chavez@gmail.com>
2019-05-06 01:50:03 -0700

    Recoded quest/etc and Scene GUI

--- Assets/Scripts/Inventory/InventoryItems.cs
+++ Assets/Scripts/Inventory/InventoryItems.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class InventoryItems : MonoBehaviour {
+public class InventoryItems {
     //item attributes
     public string itemName { get; set; }
     public string itemDescription { get; set; }
@@ -13,9 +13,9 @@ public class InventoryItems : MonoBehaviour {
     public int attackStat { get; set; }
     public Sprite sprite { get; set; }
     //item id added for use with quests
-    public int itemID { get; set; }
+    //public int itemID { get; set; }
 
-    public InventoryItems(string itemName, string itemDescription, int healthStat, int attackStat, int skillStat, Sprite icon, int itemID)
+    public InventoryItems(string itemName, string itemDescription, int healthStat, int attackStat, int skillStat, Sprite icon)
     {
         this.itemName = itemName;
         this.itemDescription = itemDescription;
@@ -23,7 +23,7 @@ public class InventoryItems : MonoBehaviour {
         this.attackStat = attackStat;
         this.skillStat = skillStat;
         this.sprite = Resources.Load<Sprite>("Art/Items/" + itemName);
-        this.itemID = itemID;
+        //this.itemID = itemID;
     }
    
  
--- Assets/Scripts/Inventory/ItemsList.cs
+++ Assets/Scripts/Inventory/ItemsList.cs
@@ -29,7 +29,7 @@ public class ItemsList : MonoBehaviour {
 
     //method that quests calls on to add a rewarded quest item to players inentory
 
-    public InventoryItems GetByItemID(int itemID)
+    /*public InventoryItems GetByItemID(int itemID)
     {
         for (int i = 0; i < item.Count; i++) {
             if (item[i].itemID == itemID) {
@@ -39,6 +39,7 @@ public class ItemsList : MonoBehaviour {
         return null;
 
     }
+    */
 
     void listOfItems()
     {
@@ -46,13 +47,13 @@ public class ItemsList : MonoBehaviour {
         item = new List<InventoryItems>()
         {
             //itemID added for quests
-            new InventoryItems("Health Potion", "Potion that raises health by 10. Can be used in and out of combat.", 10, 0, 0, Resources.Load<Sprite>("Art/Items/Health Potion"), 1),
-            new InventoryItems("Life Potion", "Potion that is used to revive a fallen member", 1000, 0,1000,Resources.Load<Sprite>("Art/Items/Life Potion"), 2),
-            new InventoryItems("Apple", "Healthy snack :3", 5, 0, 0,Resources.Load<Sprite>("Art/Items/Health Potion"), 3),
-            new InventoryItems("Juice Box", "Apple juice. Nice, refreshing, made with real juice ;)", 7, 0, 0,Resources.Load<Sprite>("Art/Items/Health Potion"), 4),
-            new InventoryItems("Grenade", "Blast your enemy away", 0, 30, 0,Resources.Load<Sprite>("Art/Items/Health Potion"), 5),
-            new InventoryItems("Demon Scroll", "Drain some of your enemy's life away", 0, 35, 0,Resources.Load<Sprite>("Art/Items/Health Potion"), 6),
-            new InventoryItems("Skill scroll", "Bump up your Skill EXP by 20 during battle", 0, 0, 20,Resources.Load<Sprite>("Art/Items/Health Potion"), 7),
+            new InventoryItems("Health Potion", "Potion that raises health by 10. Can be used in and out of combat.", 10, 0, 0, Resources.Load<Sprite>("Art/Items/Health Potion")),
+            new InventoryItems("Life Potion", "Potion that is used to revive a fallen member", 1000, 0,1000,Resources.Load<Sprite>("Art/Items/Life Potion")),
+            new InventoryItems("Apple", "Healthy snack :3", 5, 0, 0,Resources.Load<Sprite>("Art/Items/Health Potion")),
+            new InventoryItems("Juice Box", "Apple juice. Nice, refreshing, made with real juice ;)", 7, 0, 0,Resources.Load<Sprite>("Art/Items/Health Potion")),
+            new InventoryItems("Grenade", "Blast your enemy away", 0, 30, 0,Resources.Load<Sprite>("Art/Items/Health Potion")),
+            new InventoryItems("Demon Scroll", "Drain some of your enemy's life away", 0, 35, 0,Resources.Load<Sprite>("Art/Items/Health Potion")),
+            new InventoryItems("Skill scroll", "Bump up your Skill EXP by 20 during battle", 0, 0, 20,Resources.Load<Sprite>("Art/Items/Health Potion")),
         };
     }
     public void Start() {
